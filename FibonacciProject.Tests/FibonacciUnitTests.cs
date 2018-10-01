26c08f48f1cd5c3a419ec6f04e219bbbca9da558
Rivo Hermann <rahajasonr@proximity.bbdo.fr>
2018-10-01 11:54:26 +0400

    Add project files.

---
+++ FibonacciProject.Tests/FibonacciUnitTests.cs
@@ -0,0 +1,73 @@
+﻿using FibonacciTests;
+using Xunit;
+
+namespace FibonacciProject.Tests
+{
+    public class FibonacciUnitTests
+    {
+        [Fact]
+        public void Fib_Given0_Return0()
+        {
+            //arrange
+            int n = 0;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Fib_Geven1_Return1()
+        {
+            //arrange
+            int n = 1;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Geven2_Return1()
+        {
+            //arrange
+            int n = 2;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Geven3_Return2()
+        {
+            //arrange
+            int n = 3;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Fib_Geven4_Return3()
+        {
+            //arrange
+            int n = 4;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ FibonacciTests/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿namespace FibonacciTests
+{
+    public class Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            if(n==0)
+            {
+                return 0;
+            }
+
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            return Fib(n-1) + Fib(n-2);
+        }
+    }
+}
